66e6a31d87899230d18017a97f920367fc8cdf2a
A Poor Cat <82797313+poor-cat@users.noreply.github.com>
2021-09-27 20:05:55 +0530

    Update number-game.py

--- number-game.py
+++ number-game.py
@@ -66,6 +66,7 @@ for names in data_file.readlines():
     name,score,g = names.split(',')
     data_names[name] = score
     data_names2.append(name)
+
     
     if user_name in name:
         data_game_play.append(int(g))
